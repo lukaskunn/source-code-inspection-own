5596f9774a6d57332ed1a3980eb59de44bd659e0
tuzita <80728168+ivantuzita@users.noreply.github.com>
2022-09-23 22:33:12 -0300

    initial changes

--- Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
@@ -37,7 +37,7 @@ public class TicketMachine {
     }
 
     public Iterator<Integer> getTroco() {
-        return null;
+        return TrocoIterator.getIterator();
     }
 
     public String imprimir() throws SaldoInsuficienteException {
